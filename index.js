c97fe31a7d9b7a49d499981a827fcec8a9c383f8
lazyXghost <rjtbnslrealme@gmail.com>
2022-09-28 15:19:57 +0530

    Removed some bugs

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-// var canvas = document.querySelector('canvas');
-// canvas.setAttribute('class','canvas');
-// console.log(window.innerWidth); // 1550
-// console.log(window.innerHeight); // 770
-// var height = window.innerHeight;
-var pos = [793, 1420];
+const areas = {}
 $(document).ready(function() {
-    // const labels = document.getElementsByClassName('label');
-    // for(var i = 0;i<labels.length;i++){
-    //     const label = labels[i];
-    //     pos.push(label.getBoundingClientRect().y);
-    // }
-    shiftLabels();
-    addCodeforcesRank();
-});
-window.addEventListener("scroll", shiftLabels);
+    const aboutArea = document.querySelector('#aboutContent').getBoundingClientRect();
+    areas["aboutLabel"] = [aboutArea.y, aboutArea.bottom-300, 40];
+    const projectArea = document.querySelector('#projectContent').getBoundingClientRect();
+    areas["projectLabel"] = [projectArea.y, projectArea.bottom-300, 70];
+})
+window.addEventListener("scroll", shiftLabels, true);
 
-function addCodeforcesRank(){
+const windowGap = 40;
+function shiftLabel(labelClass) {
+    const label = document.getElementsByClassName(labelClass)[0];
+    const scrollY = document.body.scrollTop;
+    const labelPosMin = areas[labelClass][0];
+    const labelPosMax = areas[labelClass][1];
+    const defaultGap = areas[labelClass][2];
+
+    if(scrollY < labelPosMin - windowGap)
+        label.style.top = (defaultGap).toString() + 'px';
+    else if(scrollY < labelPosMax)
+        label.style.top = ((scrollY - labelPosMin + windowGap) + defaultGap).toString() + 'px';
+}
+
+function shiftLabels() {
+    shiftLabel('aboutLabel');
+    shiftLabel('projectLabel');
+}
+
+function addCodeforcesRank() {
     const Http = new XMLHttpRequest();
     const url = 'https://codeforces.com/api/user.info?handles=Lazyghost';
     Http.open("GET", url);
     Http.send();
-    Http.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            console.log(Http.response);
+    Http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(Http.response);
             var rank = response['result'][0]['rank'];
             var elem = document.getElementById('rank');
@@ -30,12 +40,13 @@ function addCodeforcesRank(){
         }
     }
 }
-function shiftLabels(){
-    const labels = document.getElementsByClassName('label');
-    for(var i = 0;i<labels.length;i++){
-        const label = labels[i];
-        if(scrollY > pos[i]-100 && scrollY < pos[i] + window.innerHeight - 250){
-            label.style.top = (scrollY - pos[i] + 100).toString()+"px";
-        }
-    }
+
+const displayValue = "flex";
+function toggleScroll() {
+    const slidingNavbar = document.getElementById("slidingnavbar");
+    (slidingNavbar.style.display == displayValue) ? disableScroll() : enableScroll();
+}
+function toggleNavbar() {
+    const slidingNavbar = document.getElementById("slidingnavbar");
+    slidingNavbar.style.display = (slidingNavbar.style.display == displayValue) ? "none" : displayValue;
 }
